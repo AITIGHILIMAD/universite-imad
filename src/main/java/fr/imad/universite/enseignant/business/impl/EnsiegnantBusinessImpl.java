056b12c83d7f6e2666df8b9a203d669420c713bc
59013-64-12 <59013-64-12@exchange.ad.afpanet>
2018-11-23 09:06:43 +0100

    correction

--- src/main/java/fr/imad/universite/enseignant/business/impl/EnsiegnantBusinessImpl.java
+++ src/main/java/fr/imad/universite/enseignant/business/impl/EnsiegnantBusinessImpl.java
@@ -1,59 +1,47 @@
 package fr.imad.universite.enseignant.business.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.imad.universite.enseignant.business.IEnseignantBusiness;
 import fr.imad.universite.enseignant.domaine.Enseignant;
 import fr.imad.universite.enseignant.repository.IEnseignantRepository;
+
 @Service
 public class EnsiegnantBusinessImpl implements IEnseignantBusiness {
 
 	@Autowired
-	private IEnseignantRepository  ienseignantrepository;
+	private IEnseignantRepository ienseignantrepository;
 
 	@Override
 	public List<Enseignant> getEnseignant() {
-		List<Enseignant> listeenseignant = ienseignantrepository.findAll();
-		
-		
-		return listeenseignant ;
+		return ienseignantrepository.findAll();
+
 	}
-	
-	
 
 	@Override
 	public Enseignant findEnseigant(Integer id) {
-		Enseignant E = ienseignantrepository.getOne(id);
-		return E;
+		return ienseignantrepository.getOne(id);
+
 	}
-
-
 
 	@Override
 	public void savenseignant(Enseignant enseignant) {
 		ienseignantrepository.save(enseignant);
 	}
 
-
-
 	@Override
 	public void delateenseignant(Integer id) {
 		ienseignantrepository.deleteById(id);
-		
+
 	}
 
-
-
 	@Override
-	public  Enseignant modifierenseignant(Enseignant enseignant) {
+	public Enseignant modifierenseignant(Enseignant enseignant) {
 		ienseignantrepository.save(enseignant);
 		return enseignant;
 	}
-
 
 }
